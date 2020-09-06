151ad9719c7e659adfa03d08b02f172f17fb2412
p-x9 <chi.iosdev@gmail.com>
2020-09-06 18:59:08 +0900

    Initial Commit

---
+++ p_x9-CovidApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  p_x9-CovidApp
+//
+//  Created by 千葉雅弘 on 2020/09/06.
+//  Copyright © 2020 p-x9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
